6bf587225b89f830c11726e41add8b04ad0ae3b0
Chandana702 <chandanas702@gmail.com>
2025-04-21 16:55:25 +0530

    sort error in images

--- tap_facebook/streams/ad_images.py
+++ tap_facebook/streams/ad_images.py
@@ -50,7 +50,7 @@ class AdImages(FacebookStream):
     path = f"/adimages?fields={columns}"
     tap_stream_id = "images"
     replication_method = REPLICATION_INCREMENTAL
-    replication_key = "id"
+    replication_key = "created_time"
 
     schema = PropertiesList(
         Property("id", StringType),
